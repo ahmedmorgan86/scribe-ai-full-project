20eab7c9ce12883ada1c4faddae0571136743591
Ahmed Morgan <41932609+ahmedmorgan86@users.noreply.github.com>
2026-04-09 13:42:12 +0200

    Add src/components/dashboard/index.ts

---
+++ src/components/dashboard/index.ts
@@ -0,0 +1,8 @@
+export { AgentStatus, formatLastActivity } from './AgentStatus';
+export type { AgentStatusType, AgentActivity } from './AgentStatus';
+
+export { QueueSummary } from './QueueSummary';
+export type { QueueSummaryData } from './QueueSummary';
+
+export { QuickStats, createEmptyQuickStats } from './QuickStats';
+export type { QuickStatsData } from './QuickStats';
